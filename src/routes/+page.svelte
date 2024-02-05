524e611915a417c3a97ecb379e663c1bd95a9e2f
nonorze-VOXX <appieosugood@gmail.com>
2024-02-05 23:38:24 +0800

    algin table

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,36 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-
-	// let result: string[] = ['test'];
 	let files: FileList;
 </script>
 
 <input type="file" bind:files id="fileInput" />
-<div>
+
+<table class="flex flex-col table-auto">
 	{#if files}
 		{#each Array.from(files) as file, i}
-			<p>{file.name} {file.size} bytes</p>
 			{#await file.text() then text}
-				<p>e: {text} i: {i}</p>
+				<thead>
+					{#each text[0].split('\n') as line}
+						<tr>
+							{#each line.split(',') as word}
+								<th>
+									<p>{word}</p>
+								</th>
+							{/each}
+						</tr>
+					{/each}
+				</thead>
+				<tbody>
+					{#each text.split('\n') as line}
+						<tr>
+							{#each line.split(',') as word}
+								<th>
+									<p>{word}</p>
+								</th>
+							{/each}
+						</tr>
+					{/each}
+				</tbody>
 			{/await}
 		{/each}
 	{/if}
-</div>
+</table>
