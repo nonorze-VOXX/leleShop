b1adb75cf3169f5b981862d7edb671f4c40a1846
nonorze-VOXX <appieosugood@gmail.com>
2024-02-18 00:09:12 +0800

    move logout to layout

--- src/routes/lele/+page.svelte
+++ src/routes/lele/+page.svelte
@@ -1,25 +1,2 @@
 <script lang="ts">
-	import { deserialize } from '$app/forms';
-	import { invalidate, invalidateAll } from '$app/navigation';
-	import type { ActionResult } from '@sveltejs/kit';
-
-	async function LogoutSubmit(event: { currentTarget: EventTarget & HTMLFormElement }) {
-		const data = new FormData(event.currentTarget);
-		const response = await fetch(event.currentTarget.action, {
-			method: 'POST',
-			body: data
-		});
-		const result: ActionResult = deserialize(await response.text());
-		if (result.type === 'success') {
-			invalidateAll();
-		} else {
-			console.log(result);
-		}
-	}
 </script>
-
-<div>
-	<form action="?/logout" on:submit|preventDefault={LogoutSubmit}>
-		<button type="submit">Logout</button>
-	</form>
-</div>
