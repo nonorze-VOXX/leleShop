6c85a49d1747d663cc215e119b90aae80e0e5971
nonorze-VOXX <appieosugood@gmail.com>
2024-02-18 01:13:30 +0800

    remove unuse code

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,31 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY } from '$env/static/public';
 import { type Database } from './db.types';
-import { writable } from 'svelte/store';
 
 export const supabase = createClient<Database>(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY);
-
-const userStore = writable();
-
-supabase.auth.getSession().then(({ data }) => {
-	userStore.set(data.session?.user);
-});
-
-supabase.auth.onAuthStateChange((event, session) => {
-	if (event == 'SIGNED_IN' && session) {
-		userStore.set(session.user);
-	} else if (event == 'SIGNED_OUT') {
-		userStore.set(null);
-	}
-});
 
 export type TradeHead = Database['public']['Tables']['trade_head']['Insert'];
 export type TradeBody = Database['public']['Tables']['trade_body']['Insert'];
 export type Artist = Database['public']['Tables']['artist']['Insert'];
 export default {
-	get User() {
-		return userStore;
-	},
 	async SaveArtistName(artist: Artist[]) {
 		const { error, data } = await supabase.from('artist').insert(artist).select();
 		if (error !== null) {
