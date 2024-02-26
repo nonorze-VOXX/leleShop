6ec9d09a5a86397528c4e6c30dd8c593d75b85f6
nonorze-VOXX <appieosugood@gmail.com>
2024-02-26 22:15:22 +0800

    add icon to nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,11 @@
 	import '../app.css';
 </script>
 
-<div class="bg-gray-200 min-h-screen text-red-950">
+<div class="bg-lele-bg min-h-screen text-red-950">
+	<div class="flex flex-col p-2 items-center">
+		<div class="w-24 h-24">
+			<img src="/favicon.png" alt="no icon" />
+		</div>
+	</div>
 	<slot />
 </div>
