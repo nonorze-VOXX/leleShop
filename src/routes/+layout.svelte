0ddb64cae1e34038072b9f55d9d18674b46763e8
nonorze-VOXX <appieosugood@gmail.com>
2024-02-28 02:13:59 +0800

    add icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,8 +4,8 @@
 
 <div class="bg-lele-bg min-h-screen text-red-950">
 	<div class="flex flex-col p-2 items-center">
-		<div class="w-24 h-24">
-			<img src="/favicon.png" alt="no icon" />
+		<div class="w-36 h-36">
+			<img src="/thecreateicon.png" alt="no icon" />
 		</div>
 		<div class="p-4 xl:w-3/4 lg:w-3/4 w-full">
 			<slot />
