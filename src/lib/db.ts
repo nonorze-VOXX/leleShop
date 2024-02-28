fc8d92489b62824d0d1fda6d8338dad88f92b529
nonorze-VOXX <appieosugood@gmail.com>
2024-02-29 02:53:36 +0800

    move get trade data to db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient, type QueryData } from '@supabase/supabase-js';
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY } from '$env/static/public';
 import { type Database } from './db.types';
 
@@ -7,6 +7,9 @@ export const supabase = createClient<Database>(PUBLIC_SUPABASE_URL, PUBLIC_SUPAB
 export type TradeHead = Database['public']['Tables']['trade_head']['Insert'];
 export type TradeBody = Database['public']['Tables']['trade_body']['Insert'];
 export type Artist = Database['public']['Tables']['artist']['Insert'];
+
+const QueryTradeHeadAndBody = supabase.from('trade_body').select('*, trade_head(*)');
+export type TradeBodyWithTradeHead = QueryData<typeof QueryTradeHeadAndBody>;
 export default {
 	async SaveArtistName(artist: Artist[]) {
 		const { error, data } = await supabase.from('artist').insert(artist).select();
@@ -28,5 +31,29 @@ export default {
 			console.log(error);
 		}
 		return { data, error };
+	},
+	async GetTradeData(id: string) {
+		if (id === '*' || id === '') {
+			const { data, error } = await supabase
+				.from('trade_body')
+				.select('*, trade_head(trade_id, trade_date, state)')
+				.eq('trade_head.state', '關閉');
+			if (error) {
+				console.log(error);
+			}
+
+			return data;
+		} else {
+			const { data, error } = await supabase
+				.from('trade_body')
+				.select('*, trade_head(trade_id, trade_date, state)')
+				.eq('trade_head.state', '關閉')
+				.eq('artist_id', id);
+			if (error) {
+				console.log(error);
+			}
+
+			return data;
+		}
 	}
 };
