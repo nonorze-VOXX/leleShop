1b8ab9b060750dfa3a43efa95bd9481ce1e5e123
nonorze-VOXX <appieosugood@gmail.com>
2024-03-02 19:59:06 +0800

    fix snytx progblem

--- src/routes/lele/+page.svelte
+++ src/routes/lele/+page.svelte
@@ -77,69 +77,11 @@
 		</div>
 	</div>
 {/if}
-{#if tabType === TabEnum.trade}
-	<!-- <div>trade</div>
-				<div class="max-h-screen w-full overflow-x-auto rounded-lg border-2 border-lele-line">
-					<table class="relative w-full table-fixed text-left text-sm font-medium">
-						<thead class="sticky top-0 bg-lele-line text-sm font-semibold text-lele-bg shadow-lg">
-							<th scope="col" class="w-20 px-4 py-2"> 日期 </th>
-							<th scope="col" class="w-20 px-4 py-2"> 收據號碼 </th>
-							<th scope="col" class="w-40 px-4 py-2"> 商品 </th>
-							<th scope="col" class="w-16 px-4 py-2"> 數量 </th>
-							<th scope="col" class="w-16 px-4 py-2"> 銷售總額 </th>
-							<th scope="col" class="w-16 px-4 py-2"> 折扣 </th>
-							<th scope="col" class="w-16 px-4 py-2"> 淨銷售額 </th>
-						</thead>
-
-						<tbody class="overflow-y-auto">
-							<tr
-								class=" border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-300"
-							>
-								<td></td>
-								<td></td>
-								<td class="px-4 py-2">總和</td>
-								<td class="px-4 py-2">{total_quantity}</td>
-								<td class="px-4 py-2">
-									{total}
-								</td>
-								<td class="px-4 py-2">
-									{discount_total}
-								</td>
-								<td class="px-4 py-2">
-									{net_total}
-								</td>
-							</tr>
-							{#each showedTradeDataList as trade}
-								<tr
-									class=" border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-300"
-								>
-									<td class="px-4 py-2">
-										<p>
-											{FormatDate(trade.trade_head?.trade_date)}
-										</p>
-									</td>
-									<td class="px-4 py-2"> {trade.trade_id}</td>
-									<td class="px-4 py-2"> {trade.item_name}</td>
-									<td class="px-4 py-2"> {trade.quantity}</td>
-									<td class="px-4 py-2"> {trade.total_sales}</td>
-									<td class="px-4 py-2"> {trade.discount}</td>
-									<td class="px-4 py-2"> {trade.net_sales}</td>
-								</tr>
-							{/each}
-						</tbody>
-					</table>
-				</div> -->
-{/if}
+{#if tabType === TabEnum.trade}{/if}
 {#if tabType === TabEnum.report_key}
 	<LeleDataTable
 		bind:tableHead
 		bind:tableData
 		buttonPart={{ haveButton: true, buttonText: 'update key', ButtonFunction }}
 	></LeleDataTable>
-</div>
-
-<div class="flex h-fit w-screen flex-wrap gap-4 p-4">
-	<div class="h-fit w-fit rounded-lg border-4 border-blue-700 p-3 text-5xl">
-		<a href="/lele/importCsv">import csv</a>
-	</div>
-</div>
+{/if}
