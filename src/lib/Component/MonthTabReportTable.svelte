67fb25f87b37852c1f121f83fcfd99e116d385d6
nonorze-VOXX <appieosugood@gmail.com>
2024-03-06 09:14:04 +0800

    fix css to show active month

--- src/lib/Component/MonthTabReportTable.svelte
+++ src/lib/Component/MonthTabReportTable.svelte
@@ -23,10 +23,12 @@
 	const date = new Date();
 	let firstDay: Date = new Date(date.getFullYear(), date.getMonth() - 1, 1);
 	let lastDay: Date = new Date(date.getFullYear(), date.getMonth(), 1);
+	let showedMonth: string = GetMonthToMM((firstDay.getMonth() + 1).toString());
 	const dispatch = createEventDispatcher<{
 		changeShowedDataList: { firstDay: Date; lastDay: Date };
 	}>();
 	const ClickTab = (tabData: string) => {
+		showedMonth = tabData;
 		const date = new Date();
 		firstDay = new Date(date.getFullYear(), parseInt(tabData) - 1, 1);
 		lastDay = new Date(date.getFullYear(), parseInt(tabData), 1);
@@ -35,17 +37,31 @@
 			lastDay: lastDay
 		});
 	};
-	ClickTab((firstDay.getMonth() + 1).toString());
+
+	function GetMonthToMM(month: string) {
+		return month.length === 1 ? '0' + month : month;
+	}
+	ClickTab(showedMonth);
 	$: {
 		showedTradeDataList = tradeDataList;
 	}
 </script>
 
-<div class="flex w-full flex-col gap-2">
-	<div class="flex flex-wrap justify-start gap-2 px-2">
+<div class="flex w-full flex-col">
+	<div class="mx-2 flex justify-start overflow-auto">
 		{#each tabDataList as tabData}
-			<div class="rounded-lg border-4 border-lele-line px-1 font-semibold">
-				<button on:click={() => ClickTab(tabData)}>
+			<div
+				class:bg-lele-bg={showedMonth !== tabData}
+				class:text-lele-line={showedMonth !== tabData}
+				class:bg-lele-line={showedMonth === tabData}
+				class:text-lele-bg={showedMonth === tabData}
+				class="rounded-t-xl border-l-4 border-r-4 border-t-4 border-lele-line px-1 font-semibold"
+			>
+				<button
+					on:click={() => {
+						ClickTab(tabData);
+					}}
+				>
 					{tabData}
 				</button>
 			</div>
