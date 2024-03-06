cbd057b8b52869a5edf70826eaad0ab3197ff92e
nonorze-VOXX <appieosugood@gmail.com>
2024-03-06 14:16:30 +0800

    add insource test

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import { defineConfig } from 'vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-	plugins: [sveltekit(), basicSsl()]
+	plugins: [sveltekit(), basicSsl()],
+	test: {
+		includeSource: ['src/**/*.{js,ts}']
+	},
+	define: {
+		'import.meta.vitest': 'undefined'
+	}
 });
