19baeb309bd03590725dcd605ecc87374198d691
nonorze-VOXX <appieosugood@gmail.com>
2024-03-06 14:18:32 +0800

    Revert "add insource test"
    This reverts commit cbd057b8b52869a5edf70826eaad0ab3197ff92e.

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,5 @@ import { defineConfig } from 'vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-	plugins: [sveltekit(), basicSsl()],
-	test: {
-		includeSource: ['src/**/*.{js,ts}']
-	},
-	define: {
-		'import.meta.vitest': 'undefined'
-	}
+	plugins: [sveltekit(), basicSsl()]
 });
