0a851fdfcc4077f40f54de6f3be7fbd4dc2ce06e
nonorze-VOXX <appieosugood@gmail.com>
2024-03-06 19:01:03 +0800

    move function to out side

--- src/routes/lele/importCsv/+page.server.ts
+++ src/routes/lele/importCsv/+page.server.ts
@@ -1,40 +1,7 @@
 import { fail } from '@sveltejs/kit';
-import db, { type Artist, type ArtistRow } from '$lib/db';
-import { type TradeBody, type TradeHead } from '$lib/db';
+import db from '$lib/db';
 import { groupBy } from '$lib/function/Utils';
-
-let dataHeader: string[] = [];
-const findIndex = (target: string) => {
-	return dataHeader.findLastIndex((e) => e === target);
-};
-
-const artistIndex = () => {
-	return findIndex('類別');
-};
-const itemNameIndex = () => {
-	return findIndex('商品');
-};
-const quantityIndex = () => {
-	return findIndex('數量');
-};
-const tradeIdIndex = () => {
-	return findIndex('收據號碼');
-};
-const totalIndex = () => {
-	return findIndex('銷售總額');
-};
-const discountIndex = () => {
-	return findIndex('折扣');
-};
-const netIndex = () => {
-	return findIndex('淨銷售額');
-};
-const stateIndex = () => {
-	return findIndex('狀態');
-};
-const dateIndex = () => {
-	return findIndex('日期');
-};
+import { GetNewArtistList, GetStoreData, savePartToDb, tradeIdIndex } from './importFunction';
 
 export const actions = {
 	default: async ({ request }) => {
@@ -52,13 +19,14 @@ export const actions = {
 		for (let i = 0; i < files.length; i++) {
 			const file = files[i] as File;
 			const fileArr2D = await fileToArray(file);
+			let dataHeader: string[] = [];
 			dataHeader = fileArr2D[0];
 
-			const groupByOrder = groupBy(fileArr2D.slice(1), (i) => i[tradeIdIndex()]);
+			const groupByOrder = groupBy(fileArr2D.slice(1), (i) => i[tradeIdIndex(dataHeader)]);
 
 			const tradeIdList = (await db.GetTradeIdList()).data ?? [];
 			let artistList = (await db.GetArtistDataList()).data ?? [];
-			const newArtistList = GetNewArtistList(artistList, groupByOrder);
+			const newArtistList = GetNewArtistList(artistList, groupByOrder, dataHeader);
 			{
 				if (newArtistList.length > 0) {
 					const { data } = await db.SaveArtistName(newArtistList);
@@ -70,7 +38,8 @@ export const actions = {
 				tradeIdList,
 				artistList,
 				groupByOrder,
-				timezoneOffset
+				timezoneOffset,
+				dataHeader
 			);
 			const { error } = await savePartToDb(tradeBodyList, tradeHeadList);
 			if (error !== null) {
@@ -79,84 +48,6 @@ export const actions = {
 		}
 		return true;
 	}
-};
-const GetNewArtistList = (artistList: ArtistRow[], groupByIndex: Record<string, string[][]>) => {
-	const newArtistList: Artist[] = [];
-	for (const key in groupByIndex) {
-		if (key === undefined || key === 'undefined') continue;
-		const element = groupByIndex[key];
-		for (let i = 0; i < element.length; i++) {
-			const artist_name = element[i][artistIndex()];
-			if (
-				artistList.findLastIndex((artist) => artist.artist_name === artist_name) === -1 &&
-				newArtistList.findLastIndex((artist) => artist.artist_name === artist_name) === -1
-			) {
-				newArtistList.push({ artist_name });
-			}
-		}
-	}
-	return newArtistList;
-};
-
-const GetStoreData = (
-	tradeIdList: { trade_id: string }[],
-	artistList: ArtistRow[],
-	groupByIndex: Record<string, string[][]>,
-	timezoneOffset: string
-) => {
-	const tradeBodyList: TradeBody[] = [];
-	const tradeHeadList: TradeHead[] = [];
-
-	for (const key in groupByIndex) {
-		if (key === undefined || key === 'undefined') continue;
-		if (tradeIdList.findLastIndex((i) => i.trade_id === key) !== -1) {
-			console.log('DB had it');
-			continue;
-		}
-
-		const element = groupByIndex[key];
-		const date = new Date(element[0][dateIndex()].replace(/ /g, 'T') + timezoneOffset);
-
-		tradeHeadList.push({
-			trade_date: date.toISOString(),
-			trade_id: element[0][tradeIdIndex()],
-			state: element[0][stateIndex()]
-		});
-
-		for (let i = 0; i < element.length; i++) {
-			const artist_name = element[i][artistIndex()];
-			if (artistList.findLastIndex((artist) => artist.artist_name === artist_name) === -1) {
-				console.error('artist not found', artist_name);
-			}
-			const artist_id = artistList.find((artist) => artist.artist_name === artist_name)?.id;
-
-			tradeBodyList.push({
-				item_name: element[i][itemNameIndex()],
-				quantity: parseInt(element[i][quantityIndex()]),
-				trade_id: element[i][tradeIdIndex()],
-				total_sales: parseFloat(element[i][totalIndex()]),
-				discount: parseFloat(element[i][discountIndex()]),
-				net_sales: parseFloat(element[i][netIndex()]),
-				artist_id: parseInt(artist_id as unknown as string)
-			});
-		}
-	}
-	return { tradeBodyList, tradeHeadList };
-};
-const savePartToDb = async (tradeBodyList: TradeBody[], tradeHeadList: TradeHead[]) => {
-	{
-		const { error } = await db.SaveTradeHead(tradeHeadList);
-		if (error !== null) {
-			return { error };
-		}
-	}
-	{
-		const { error } = await db.SaveTradeBody(tradeBodyList);
-		if (error !== null) {
-			return { error };
-		}
-	}
-	return { error: null };
 };
 
 const fileToArray = async (file: File) => {
---
+++ src/routes/lele/importCsv/importFunction.ts
@@ -0,0 +1,117 @@
+import type { Artist, ArtistRow, TradeBody, TradeHead } from '$lib/db';
+import db from '$lib/db';
+
+export const findIndex = (dataHeader: string[], target: string) => {
+	return dataHeader.findLastIndex((e) => e === target);
+};
+
+export const artistIndex = (dataHeader: string[]) => {
+	return findIndex(dataHeader, '類別');
+};
+export const itemNameIndex = (dataHeader: string[]) => {
+	return findIndex(dataHeader, '商品');
+};
+export const quantityIndex = (dataHeader: string[]) => {
+	return findIndex(dataHeader, '數量');
+};
+export const tradeIdIndex = (dataHeader: string[]) => {
+	return findIndex(dataHeader, '收據號碼');
+};
+export const totalIndex = (dataHeader: string[]) => {
+	return findIndex(dataHeader, '銷售總額');
+};
+export const discountIndex = (dataHeader: string[]) => {
+	return findIndex(dataHeader, '折扣');
+};
+export const netIndex = (dataHeader: string[]) => {
+	return findIndex(dataHeader, '淨銷售額');
+};
+export const stateIndex = (dataHeader: string[]) => {
+	return findIndex(dataHeader, '狀態');
+};
+export const dateIndex = (dataHeader: string[]) => {
+	return findIndex(dataHeader, '日期');
+};
+export const GetNewArtistList = (
+	artistList: ArtistRow[],
+	groupByIndex: Record<string, string[][]>,
+	dataHeader: string[]
+) => {
+	const newArtistList: Artist[] = [];
+	for (const key in groupByIndex) {
+		if (key === undefined || key === 'undefined') continue;
+		const element = groupByIndex[key];
+		for (let i = 0; i < element.length; i++) {
+			const artist_name = element[i][artistIndex(dataHeader)];
+			if (
+				artistList.findLastIndex((artist) => artist.artist_name === artist_name) === -1 &&
+				newArtistList.findLastIndex((artist) => artist.artist_name === artist_name) === -1
+			) {
+				newArtistList.push({ artist_name });
+			}
+		}
+	}
+	return newArtistList;
+};
+
+export const GetStoreData = (
+	tradeIdList: { trade_id: string }[],
+	artistList: ArtistRow[],
+	groupByIndex: Record<string, string[][]>,
+	timezoneOffset: string,
+	dataHeader: string[]
+) => {
+	const tradeBodyList: TradeBody[] = [];
+	const tradeHeadList: TradeHead[] = [];
+
+	for (const key in groupByIndex) {
+		if (key === undefined || key === 'undefined') continue;
+		if (tradeIdList.findLastIndex((i) => i.trade_id === key) !== -1) {
+			console.log('DB had it');
+			continue;
+		}
+
+		const element = groupByIndex[key];
+		const date = new Date(element[0][dateIndex(dataHeader)].replace(/ /g, 'T') + timezoneOffset);
+
+		tradeHeadList.push({
+			trade_date: date.toISOString(),
+			trade_id: element[0][tradeIdIndex(dataHeader)],
+			state: element[0][stateIndex(dataHeader)]
+		});
+
+		for (let i = 0; i < element.length; i++) {
+			const artist_name = element[i][artistIndex(dataHeader)];
+			if (artistList.findLastIndex((artist) => artist.artist_name === artist_name) === -1) {
+				console.error('artist not found', artist_name);
+			}
+			const artist_id = artistList.find((artist) => artist.artist_name === artist_name)?.id;
+
+			tradeBodyList.push({
+				item_name: element[i][itemNameIndex(dataHeader)],
+				quantity: parseInt(element[i][quantityIndex(dataHeader)]),
+				trade_id: element[i][tradeIdIndex(dataHeader)],
+				total_sales: parseFloat(element[i][totalIndex(dataHeader)]),
+				discount: parseFloat(element[i][discountIndex(dataHeader)]),
+				net_sales: parseFloat(element[i][netIndex(dataHeader)]),
+				artist_id: parseInt(artist_id as unknown as string)
+			});
+		}
+	}
+	return { tradeBodyList, tradeHeadList };
+};
+export const savePartToDb = async (tradeBodyList: TradeBody[], tradeHeadList: TradeHead[]) => {
+	{
+		const { error } = await db.SaveTradeHead(tradeHeadList);
+		if (error !== null) {
+			return { error };
+		}
+	}
+	{
+		const { error } = await db.SaveTradeBody(tradeBodyList);
+		if (error !== null) {
+			return { error };
+		}
+	}
+	return { error: null };
+};
