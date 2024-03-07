b57dea41b96f1e84b64f6c41d87b63a7b68f3da5
nonorze-VOXX <appieosugood@gmail.com>
2024-03-07 12:01:22 +0800

    add more test to format date

--- src/lib/function/Utils.test.ts
+++ src/lib/function/Utils.test.ts
@@ -3,7 +3,10 @@ import { FormatDate } from './Utils';
 
 test.each([
 	{ dateStr: '2024-01-01 19:00', answer: '2024/01/01' },
-	{ dateStr: '2024-01-01Z19:00+08:00', answer: '2024/01/01' }
+	{ dateStr: '2024-01-02Z01:00+08:00', answer: '2024/01/02' },
+	{ dateStr: '2024-01-01Z19:00+08:00', answer: '2024/01/01' },
+	{ dateStr: '2024-01-01T17:00:00.000Z', answer: '2024/01/02' },
+	{ dateStr: '2024-01-01T10:00:00.000Z', answer: '2024/01/01' }
 ])('format date $dateStr', ({ dateStr, answer }) => {
 	const result = FormatDate(dateStr);
 	expect(result).toBe(answer);
