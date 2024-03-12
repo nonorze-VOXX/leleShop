3060926df9851bef4353721f6445411f907b50e5
nonorze-VOXX <appieosugood@gmail.com>
2024-03-13 00:11:26 +0800

    add some footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import '../app.css';
 </script>
 
-<div class="min-h-screen bg-lele-bg text-red-950">
+<div class="flex min-h-screen flex-col justify-between bg-lele-bg text-red-950">
 	<div class="flex flex-col items-center p-2">
 		<div class="h-36 w-36">
 			<a href="/">
@@ -13,4 +13,9 @@
 			<slot />
 		</div>
 	</div>
+	<footer>
+		<div class=" w-full bg-lele-line p-4 text-center font-semibold text-lele-bg">
+			這個網頁還在開發中，僅供參考 如果有bug 或是想要新功能 請回報給管理員
+		</div>
+	</footer>
 </div>
