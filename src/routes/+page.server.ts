16c6e8ad81b0532e24e43ededefd4109d69722c6
nonorze-VOXX <appieosugood@gmail.com>
2024-04-07 01:12:51 +0800

    fix hardcode

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,12 @@
 import { supabase } from '$lib/db';
+import { GetNowSeason } from '$lib/function/Utils';
 
 export const load = async () => {
 	let query = supabase.from('artist').select('id, artist_name, visible,artist_payment_status(*)');
 
 	query = query
 		.eq('visible', true)
-		.eq('artist_payment_status.season', '2024-03')
+		.eq('artist_payment_status.season', GetNowSeason())
 		.order('id', { ascending: true });
 	const { data, error } = await query;
 	if (error) {
