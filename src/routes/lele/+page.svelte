114f66e4552538be357737a92f47a5d418e02852
nonorze-VOXX <appieosugood@gmail.com>
2024-04-06 17:12:24 +0800

    make disable

--- src/routes/lele/+page.svelte
+++ src/routes/lele/+page.svelte
@@ -68,6 +68,8 @@
 			console.log('succ');
 		} else if (result.type === 'failure') {
 			console.log(result.data);
+		} else if (result.type === 'redirect') {
+			goto(result.location);
 		}
 	};
 	const ButtonFunction = async (value: string[]) => {
@@ -253,14 +255,15 @@
 								<input
 									type="checkbox"
 									checked={paymentData.process_state === 'done'}
-									disabled={false && paymentData.process_state === 'todo'}
+									disabled={paymentData.process_state === 'todo'}
 									on:change={() => {
 										UpdatePaymentStatus(paymentData);
 									}}
 									class="peer sr-only"
 								/>
 								<div
-									class="peer relative z-10 h-6 w-11 rounded-full bg-gray-200 after:absolute after:start-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rtl:peer-checked:after:-translate-x-full dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800"
+									class:opacity-50={paymentData.process_state === 'todo'}
+									class="peer relative z-10 h-6 w-11 rounded-full bg-gray-200 after:absolute after:start-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-0 peer-focus:ring-blue-300 rtl:peer-checked:after:-translate-x-full dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800"
 								></div>
 							</label>
 						</td>
