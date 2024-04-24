28e387bc1df0b45612e896315e1911dede1b1279
nonorze-VOXX <appieosugood@gmail.com>
2024-04-24 09:07:07 +0800

    extra component

--- src/routes/lele/+page.svelte
+++ src/routes/lele/+page.svelte
@@ -7,14 +7,9 @@
 	import { deserialize } from '$app/forms';
 	import type { ActionResult } from '@sveltejs/kit';
 	import { goto, invalidateAll } from '$app/navigation';
-	import type { Artist, ArtistRow, PaymentStatusRow, QueryTradeBodyWithTradeHead } from '$lib/db';
+	import type { ArtistRow, PaymentStatusRow, QueryTradeBodyWithTradeHead } from '$lib/db';
 	import MonthTabReportTable from '$lib/Component/MonthTabReportTable.svelte';
-	import LeleTable from '$lib/Component/htmlWrapper/LeleTable.svelte';
-	import LeleThead from '$lib/Component/htmlWrapper/LeleThead.svelte';
-	import LeleTbody from '$lib/Component/htmlWrapper/LeleTbody.svelte';
-	import LeleTbodyTr from '$lib/Component/htmlWrapper/LeleTbodyTr.svelte';
-	import { FormatNumberToTwoDigi } from '$lib/function/Utils';
-	import Layout from '../+layout.svelte';
+	import PaymentTable from './PaymentTable.svelte';
 
 	export let data: PageData;
 
@@ -42,7 +37,6 @@
 		tableData = data.artistData?.map((artist) => {
 			return [artist.artist_name, artist.report_key];
 		}) as string[][];
-		// tradeDataList = data.tradeDataList as QueryTradeBodyWithTradeHead;
 		const date = new Date();
 		let firstDay: Date = new Date(date.getFullYear(), date.getMonth() - 1, 1);
 		let lastDay: Date = new Date(date.getFullYear(), date.getMonth(), 1);
@@ -51,40 +45,6 @@
 		paymentDataList = data.paymentStatus;
 		await UpdateTradeData(firstDay, lastDay);
 	});
-	const UpdatePaymentStatus = async (paymentData: PaymentStatusRow) => {
-		const data = new FormData();
-
-		if (
-			paymentData.year_month === null ||
-			paymentData.process_state === null ||
-			paymentData.artist_id === null ||
-			paymentData.id === null
-		) {
-			console.log('data error please connect to developer');
-			return;
-		}
-		const state = paymentData.process_state === 'done' ? 'todo' : 'done';
-		console.log(paymentData.year_month);
-
-		data.append('season', paymentData.year_month);
-		data.append('process_state', state);
-		data.append('artist_id', paymentData.artist_id.toString());
-		data.append('payment_id', paymentData.id.toString());
-
-		const response = await fetch('?/UpdatePaymentStatus', {
-			method: 'POST',
-			body: data
-		});
-		const result = deserialize(await response.text());
-		console.log(result);
-		if (result.type === 'success') {
-			console.log('succ');
-		} else if (result.type === 'failure') {
-			console.log(result.data);
-		} else if (result.type === 'redirect') {
-			goto(result.location);
-		}
-	};
 	const ButtonFunction = async (value: string[]) => {
 		const data = new FormData();
 		const artist = artistData.find((e) => e.artist_name == value[0]);
@@ -181,47 +141,7 @@
 	></LeleDataTable>
 {/if}
 {#if tabType === TabEnum.payment}
-	<LeleTable>
-		<LeleThead>
-			<tr>
-				<th scope="col" class="w-auto p-2"> 品牌 </th>
-				<th scope="col" class="w-40 p-2"> 繳交狀況</th>
-			</tr>
-		</LeleThead>
-		<LeleTbody>
-			{#if paymentDataList}
-				{#each paymentDataList as p}
-					<LeleTbodyTr>
-						<td class="p-2">
-							{p.artist_name}
-						</td>
-						<td>
-							<div class="flex flex-col justify-around gap-2 py-2">
-								{#each p.artist_payment_status as pay}
-									<div class="flex justify-start gap-2 align-baseline">
-										<div class="">
-											{pay.year_month}
-										</div>
-										<label class="inline-flex cursor-pointer items-center">
-											<input
-												type="checkbox"
-												checked={pay.process_state === 'done'}
-												on:change={() => {
-													UpdatePaymentStatus(pay);
-												}}
-												class="peer sr-only"
-											/>
-											<div
-												class="peer relative z-10 h-6 w-11 rounded-full bg-gray-200 after:absolute after:start-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-0 peer-focus:ring-blue-300 rtl:peer-checked:after:-translate-x-full dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800"
-											></div>
-										</label>
-									</div>
-								{/each}
-							</div>
-						</td>
-					</LeleTbodyTr>
-				{/each}
-			{/if}
-		</LeleTbody>
-	</LeleTable>
+	{#if paymentDataList}
+		<PaymentTable bind:paymentDataList></PaymentTable>
+	{/if}
 {/if}
---
+++ src/routes/lele/PaymentTable.svelte
@@ -0,0 +1,93 @@
+<script lang="ts">
+	import { deserialize } from '$app/forms';
+	import { goto } from '$app/navigation';
+	import type { PaymentStatusRow } from '$lib/db';
+	import LeleTable from '$lib/Component/htmlWrapper/LeleTable.svelte';
+	import LeleThead from '$lib/Component/htmlWrapper/LeleThead.svelte';
+	import LeleTbody from '$lib/Component/htmlWrapper/LeleTbody.svelte';
+	import LeleTbodyTr from '$lib/Component/htmlWrapper/LeleTbodyTr.svelte';
+
+	export let paymentDataList: {
+		id: number;
+		artist_name: string | null;
+		visible: boolean;
+		artist_payment_status: PaymentStatusRow[];
+	}[] = [];
+
+	const UpdatePaymentStatus = async (paymentData: PaymentStatusRow) => {
+		const data = new FormData();
+
+		if (
+			paymentData.year_month === null ||
+			paymentData.process_state === null ||
+			paymentData.artist_id === null ||
+			paymentData.id === null
+		) {
+			console.log('data error please connect to developer');
+			return;
+		}
+		const state = paymentData.process_state === 'done' ? 'todo' : 'done';
+		console.log(paymentData.year_month);
+
+		data.append('season', paymentData.year_month);
+		data.append('process_state', state);
+		data.append('artist_id', paymentData.artist_id.toString());
+		data.append('payment_id', paymentData.id.toString());
+
+		const response = await fetch('?/UpdatePaymentStatus', {
+			method: 'POST',
+			body: data
+		});
+		const result = deserialize(await response.text());
+		console.log(result);
+		if (result.type === 'success') {
+			console.log('succ');
+		} else if (result.type === 'failure') {
+			console.log(result.data);
+		} else if (result.type === 'redirect') {
+			goto(result.location);
+		}
+	};
+</script>
+
+<LeleTable>
+	<LeleThead>
+		<tr>
+			<th scope="col" class="w-auto p-2"> 品牌 </th>
+			<th scope="col" class="w-40 p-2"> 繳交狀況</th>
+		</tr>
+	</LeleThead>
+	<LeleTbody>
+		{#each paymentDataList as p}
+			<LeleTbodyTr>
+				<td class="p-2">
+					{p.artist_name}
+				</td>
+				<td>
+					<div class="flex flex-col justify-around gap-2 py-2">
+						{#each p.artist_payment_status as pay}
+							<div class="flex justify-start gap-2 align-baseline">
+								<div class="">
+									{pay.year_month}
+								</div>
+								<label class="inline-flex cursor-pointer items-center">
+									<input
+										type="checkbox"
+										checked={pay.process_state === 'done'}
+										on:change={() => {
+											UpdatePaymentStatus(pay);
+										}}
+										class="peer sr-only"
+									/>
+									<div
+										class="peer relative z-10 h-6 w-11 rounded-full bg-gray-200 after:absolute after:start-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-0 peer-focus:ring-blue-300 rtl:peer-checked:after:-translate-x-full dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800"
+									></div>
+								</label>
+							</div>
+						{/each}
+					</div>
+				</td>
+			</LeleTbodyTr>
+		{/each}
+	</LeleTbody>
+</LeleTable>
