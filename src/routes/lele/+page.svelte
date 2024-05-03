4c8ebedfe59592c3c27493dc9e35b525059476c1
nonorze-VOXX <appieosugood@gmail.com>
2024-05-03 18:08:05 +0800

    move out admin trade table

--- src/routes/lele/+page.svelte
+++ src/routes/lele/+page.svelte
@@ -1,16 +1,14 @@
 <script lang="ts">
+	import AdminTradeTable from './AdminTradeTable.svelte';
 	import ReportKeyTable from './ReportKeyTable.svelte';
-
 	import ArtistListPart from './ArtistListPart.svelte';
 
 	import { onMount } from 'svelte';
-	import type { ArtistRow, PaymentStatusRow, QueryTradeBodyWithTradeHead } from '$lib/db';
-	import MonthTabReportTable from '$lib/Component/MonthTabReportTable.svelte';
+	import type { ArtistRow } from '$lib/db';
 	import PaymentTable from './PaymentTable.svelte';
-	import db, { supabase } from '$lib/db';
+	import db from '$lib/db';
 
 	let artistData: ArtistRow[] = [];
-	let tradeDataList: QueryTradeBodyWithTradeHead;
 	enum TabEnum {
 		artist_list,
 		trade,
@@ -20,29 +18,7 @@
 	let tabType: TabEnum = TabEnum.artist_list;
 	onMount(async () => {
 		artistData = (await db.GetArtistDataList())?.data ?? [];
-		const date = new Date();
-		let firstDay: Date = new Date(date.getFullYear(), date.getMonth() - 1, 1);
-		let lastDay: Date = new Date(date.getFullYear(), date.getMonth(), 1);
-
-		await UpdateTradeData(firstDay, lastDay);
 	});
-	let showedLength = 0;
-	const UpdateTradeData = async (firstDate: Date, lastDate: Date) => {
-		const tradeData = (
-			await db.GetTradeData('*', {
-				firstDate: new Date(firstDate),
-				lastDate: new Date(lastDate)
-			})
-		).data;
-
-		const { count } = await db.GetTradeDataCount('*', {
-			firstDate: firstDate,
-			lastDate: lastDate
-		});
-
-		tradeDataList = tradeData;
-		showedLength = count ?? -1;
-	};
 </script>
 
 <div class="flex justify-start gap-2 p-2">
@@ -74,19 +50,7 @@
 	<ArtistListPart bind:artistData></ArtistListPart>
 {/if}
 {#if tabType === TabEnum.trade}
-	<div class="flex flex-col gap-2">
-		<div class="flex px-2">
-			<div class="rounded-xl bg-lele-line p-2 text-lele-bg">
-				交易次數：{showedLength}
-			</div>
-		</div>
-		<MonthTabReportTable
-			bind:tradeDataList
-			on:changeShowedDataList={(e) => {
-				UpdateTradeData(e.detail.firstDay, e.detail.lastDay);
-			}}
-		></MonthTabReportTable>
-	</div>
+	<AdminTradeTable></AdminTradeTable>
 {/if}
 {#if tabType === TabEnum.report_key}
 	<ReportKeyTable bind:artistData></ReportKeyTable>
