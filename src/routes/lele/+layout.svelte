390d9fb313ff6ffcc825bfcfbeca73908013b0a6
nonorze-VOXX <appieosugood@gmail.com>
2024-05-09 00:49:39 +0800

    rm let

--- src/routes/lele/+layout.svelte
+++ src/routes/lele/+layout.svelte
@@ -1,16 +1,13 @@
 <script lang="ts">
 	import { supabase } from '$lib/db';
 	import { onMount } from 'svelte';
-	import { browser } from '$app/environment';
 	import { goto } from '$app/navigation';
-
-	export let redirect: string = '/login';
 
 	onMount(async () => {
 		const access_token = (await supabase.auth.getSession()).data.session?.access_token;
 		if (access_token === undefined || access_token === '') {
 			console.log('access token ', access_token);
-			goto(redirect, { replaceState: true });
+			goto('/login', { replaceState: true });
 		}
 	});
 
