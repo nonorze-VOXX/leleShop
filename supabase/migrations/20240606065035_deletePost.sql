1413df7daed485c5b4bc9b84669aefb8433643f4
nonorze-VOXX <appieosugood@gmail.com>
2024-06-06 14:51:48 +0800

    delete post

---
+++ supabase/migrations/20240606065035_deletePost.sql
@@ -0,0 +1,54 @@
+drop policy "Enable insert for authenticated users only" on "public"."posts";
+
+drop policy "Enable read access for all users" on "public"."posts";
+
+revoke delete on table "public"."posts" from "anon";
+
+revoke insert on table "public"."posts" from "anon";
+
+revoke references on table "public"."posts" from "anon";
+
+revoke select on table "public"."posts" from "anon";
+
+revoke trigger on table "public"."posts" from "anon";
+
+revoke truncate on table "public"."posts" from "anon";
+
+revoke update on table "public"."posts" from "anon";
+
+revoke delete on table "public"."posts" from "authenticated";
+
+revoke insert on table "public"."posts" from "authenticated";
+
+revoke references on table "public"."posts" from "authenticated";
+
+revoke select on table "public"."posts" from "authenticated";
+
+revoke trigger on table "public"."posts" from "authenticated";
+
+revoke truncate on table "public"."posts" from "authenticated";
+
+revoke update on table "public"."posts" from "authenticated";
+
+revoke delete on table "public"."posts" from "service_role";
+
+revoke insert on table "public"."posts" from "service_role";
+
+revoke references on table "public"."posts" from "service_role";
+
+revoke select on table "public"."posts" from "service_role";
+
+revoke trigger on table "public"."posts" from "service_role";
+
+revoke truncate on table "public"."posts" from "service_role";
+
+revoke update on table "public"."posts" from "service_role";
+
+alter table "public"."posts" drop constraint "posts_pkey";
+
+drop index if exists "public"."posts_pkey";
+
+drop table "public"."posts";
+
+
+
