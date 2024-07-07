d55622a4c101ac7b288d9ceb848715917d2ce03e
nonorze-VOXX <appieosugood@gmail.com>
2024-07-07 23:39:56 +0800

    rm time zone problem

--- src/lib/function/time.test.ts
+++ src/lib/function/time.test.ts
@@ -23,22 +23,10 @@ test('test date 0 or cross year', () => {
 
 test('format from lele', () => {
 	{
-		let date = new Date('2024-05-31 21:51+08');
-		expect(date.getFullYear()).toBe(2024);
-		expect(date.getMonth()).toBe(4);
-		expect(date.getDate()).toBe(31);
-		expect(date.getHours()).toBe(21);
-		expect(date.toISOString()).toBe('2024-05-31T13:51:00.000Z');
-	}
-	{
 		let timezoneOffset = new Date().getTimezoneOffset();
 		let HHMM = timeZoneOffsetToHHMM(timezoneOffset);
 		expect(HHMM).toBe('+08:00');
 		let date = GetDateWithTimeZone('2024-05-31 21:51', HHMM);
-		expect(date.getFullYear()).toBe(2024);
-		expect(date.getMonth()).toBe(4);
-		expect(date.getDate()).toBe(31);
-		expect(date.getHours()).toBe(21);
 		expect(date.toISOString()).toBe('2024-05-31T13:51:00.000Z');
 	}
 });
