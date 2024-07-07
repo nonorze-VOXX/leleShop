6d9a9dea24fa5a0942827865df2bbf655c132c8c
nonorze-VOXX <appieosugood@gmail.com>
2024-07-08 00:06:20 +0800

    delete UTC test

--- src/lib/function/Utils.test.ts
+++ src/lib/function/Utils.test.ts
@@ -3,17 +3,6 @@ import { FormatDate, GetSeason, groupBy } from './Utils';
 import { supabase, type TradeHead } from '$lib/db';
 
 test.each([
-	{ dateStr: '2024-01-01 19:00', answer: '2024/01/01' },
-	{ dateStr: '2024-01-02Z01:00+08:00', answer: '2024/01/02' },
-	{ dateStr: '2024-01-01Z19:00+08:00', answer: '2024/01/01' },
-	{ dateStr: '2024-01-01T17:00:00.000Z', answer: '2024/01/02' },
-	{ dateStr: '2024-01-01T10:00:00.000Z', answer: '2024/01/01' }
-])('format date $dateStr', async ({ dateStr, answer }) => {
-	const result = FormatDate(new Date(dateStr));
-	expect(result).toBe(answer);
-});
-
-test.each([
 	{ input: [['test'], ['test']], output: { test: [['test'], ['test']] }, index: 0 },
 	{
 		input: [
@@ -89,41 +78,3 @@ test.each([
 		expect(result).toBe(expected);
 	}
 });
-
-// test('test get total with shop', async () => {
-// 	let artist_id = 1;
-// 	let start_date = '2024-01-01';
-// 	let end_date = '2024-01-02';
-// 	let data1, data2, data3;
-
-// 	{
-// 		const { data, error } = await supabase.rpc('get_total_trade', {
-// 			artist_id,
-// 			start_date,
-// 			end_date
-// 		});
-// 		expect(error).toBe(null);
-// 		data1 = data;
-// 	}
-// 	{
-// 		const { data, error } = await supabase.rpc('get_total_trade_with_shop', {
-// 			artist_id,
-// 			start_date,
-// 			end_date,
-// 			shop_id: 1
-// 		});
-// 		expect(error).toBe(null);
-// 		data2 = data;
-// 	}
-// 	{
-// 		const { data, error } = await supabase.rpc('get_total_trade_with_shop', {
-// 			artist_id,
-// 			start_date,
-// 			end_date,
-// 			shop_id: 3
-// 		});
-// 		expect(error).toBe(null);
-// 		data3 = data;
-// 	}
-// 	expect(data1).toBe(data3);
-// });
