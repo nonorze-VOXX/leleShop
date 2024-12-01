fa60ab7a6f268130cd3a74fa8305931937530715
nonorze-VOXX <appieosugood@gmail.com>
2024-12-01 22:05:26 +0800

    move view eidt

--- supabase/migrations/20241127141306_createStore.sql
+++ supabase/migrations/20241127141306_createStore.sql
@@ -60,3 +60,23 @@ grant truncate on table "public"."store" to "service_role";
 grant update on table "public"."store" to "service_role";
 
 insert into store (store_name) values ('The創 一中');
+
+create or replace  view 
+  public.artist_trade  WITH ("security_invoker"='on')  as
+select
+  a.artist_name,
+  b.id,
+  b.trade_id,
+  b.item_name,
+  b.quantity,
+  b.total_sales,
+  b.discount,
+  b.net_sales,
+  b.artist_id,
+  h.trade_date,
+  s.store_name
+from
+  artist a
+  join trade_body b on a.id = b.artist_id
+  join trade_head h on b.trade_id = h.trade_id
+  left join store s on s.id = h.store_id;
