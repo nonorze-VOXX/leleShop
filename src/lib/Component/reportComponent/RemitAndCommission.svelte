44b5cd4e7544dbc4ca21936366fe7fef86aaa056
nonorze-VOXX <appieosugood@gmail.com>
2024-12-26 23:55:17 +0800

    remit update when year month change

--- src/lib/Component/reportComponent/RemitAndCommission.svelte
+++ src/lib/Component/reportComponent/RemitAndCommission.svelte
@@ -2,7 +2,8 @@
 	import { browser } from '$app/environment';
 	import { supabase } from '$lib/db';
 	import { selectedStore } from '$lib/store/choosing';
-	import { onDestroy } from 'svelte';
+	import { onDestroy, onMount } from 'svelte';
+
 	import Remit from './Remit.svelte';
 	import Commission from './Commission.svelte';
 
@@ -11,18 +12,18 @@
 	export let year_month: string;
 	let commission: number | null = null;
 
-	const unsubscribe = selectedStore.subscribe(async (e) => {
+	const fetchCommission = async () => {
 		if (browser) {
-			if (e !== '*' && e.length === 1) {
+			if ($selectedStore !== '*' && $selectedStore.length === 1) {
 				const { data, error } = await supabase
 					.from('default_commission_view')
 					.select('*')
-					.eq('store_name', e[0])
+					.eq('store_name', $selectedStore[0])
 					.eq('artist_id', artist_id)
 					.eq('year_month', year_month)
 					.single();
 				if (error) {
-					console.error(error);
+					// console.error(error);
 					commission = null;
 					return;
 				}
@@ -31,7 +32,14 @@
 				commission = null;
 			}
 		}
-	});
+	};
+
+	const unsubscribe = selectedStore.subscribe(fetchCommission);
+
+	onMount(fetchCommission);
+
+	$: year_month, fetchCommission();
+	$: net_total, fetchCommission();
 
 	onDestroy(unsubscribe);
 </script>
