8e9cd19c56226d25fc17163f34b8747e66cba6a2
nonorze-VOXX <appieosugood@gmail.com>
2025-02-27 16:08:17 +0800

    extra more function

--- src/routes/lele/import/importFunction.ts
+++ src/routes/lele/import/importFunction.ts
@@ -150,68 +150,17 @@ export const ProcessFile = async (file: File) => {
 
 	await saveNotExistArtist(not_exist_artist);
 
-	const artistList = await GetArtistList();
-	{
-		const storePreData = await supabase.from('store').select();
-		if (storePreData.error) {
-			throw new Error(storePreData.error.message);
-		}
+	await PreInsertStores(importedTrade);
+	const storeData = await GetStoreDataFromDb();
 
-		const storeSet = GetStoreSet(importedTrade);
-		const notExistStore = [...storeSet].filter((e) => {
-			return !storePreData.data.some((store) => store.store_name === e);
-		});
-		const saveStore = await supabase
-			.from('store')
-			.insert(
-				notExistStore.map((e) => {
-					return { store_name: e };
-				})
-			)
-			.select();
-		if (saveStore.error) {
-			throw new Error(saveStore.error.message);
-		}
-	}
-	const storeData = await supabase.from('store').select();
-	if (storeData.error) {
-		throw new Error(storeData.error.message);
-	}
-
-	const tradeHeadSet = GetTradeHeadSet(importedTrade, storeData.data ?? []);
-	const tradeHeadList = [...tradeHeadSet].map((e) => {
-		return e.trade_id;
-	});
-	const existTradeHead: {
-		store_id: number;
-		trade_date: string;
-		trade_id: string;
-	}[] = [];
-
-	const partLen = 300;
-	for (let i = 0; i < tradeHeadList.length; i += partLen) {
-		const existTradeHeadPart = await supabase
-			.from('trade_head')
-			.select()
-			.in('trade_id', tradeHeadList.slice(i, i + partLen));
-		if (existTradeHeadPart.error) {
-			throw new Error(existTradeHeadPart.error.message);
-		} else {
-			// existTradeHead += existTradeHeadPart.data??[];
-			existTradeHeadPart.data?.forEach((e) => {
-				existTradeHead.push(e);
-			});
-		}
-	}
-
-	const noDupTradeHead = [...tradeHeadSet].filter((e) => {
-		return !existTradeHead.some((tradeHead) => tradeHead.trade_id === e.trade_id);
-	});
+	const noDupTradeHead = await BiggerGetNoDupTradeHead(importedTrade, storeData);
 
 	const saveHead = await db.SaveTradeHead(noDupTradeHead);
 	if (saveHead.error) {
 		throw new Error(saveHead.error.message);
 	}
+
+	const artistList = await GetArtistList();
 	const tradeBodyList: TradeBody[] = importedTrade
 		.map((e) => {
 			const al = artistList.data ?? [];
@@ -251,6 +200,81 @@ export const GetArtistNameList = (data: ImportedTrade[]) => {
 	});
 	return artistNameSet;
 };
+
+async function BiggerGetNoDupTradeHead(
+	importedTrade: ImportedTrade[],
+	storeData: { created_at: string; id: number; store_name: string }[]
+) {
+	const tradeHeadSet = GetTradeHeadSet(importedTrade, storeData ?? []);
+	const tradeHeadList = [...tradeHeadSet].map((e) => {
+		return e.trade_id;
+	});
+	const noDupTradeHead = await GetNoDupTradeHead(tradeHeadList, tradeHeadSet);
+	return noDupTradeHead;
+}
+
+async function GetStoreDataFromDb() {
+	const storeData = await supabase.from('store').select();
+	if (storeData.error) {
+		throw new Error(storeData.error.message);
+	}
+	return storeData.data;
+}
+
+async function PreInsertStores(importedTrade: ImportedTrade[]) {
+	const storePreData = await supabase.from('store').select();
+	if (storePreData.error) {
+		throw new Error(storePreData.error.message);
+	}
+
+	const storeSet = GetStoreSet(importedTrade);
+	const notExistStore = [...storeSet].filter((e) => {
+		return !storePreData.data.some((store) => store.store_name === e);
+	});
+	const saveStore = await supabase
+		.from('store')
+		.insert(
+			notExistStore.map((e) => {
+				return { store_name: e };
+			})
+		)
+		.select();
+	if (saveStore.error) {
+		throw new Error(saveStore.error.message);
+	}
+}
+
+async function GetNoDupTradeHead(
+	tradeHeadList: string[],
+	tradeHeadSet: Set<{ store_id: number; trade_date: string; trade_id: string }>
+) {
+	const existTradeHead: {
+		store_id: number;
+		trade_date: string;
+		trade_id: string;
+	}[] = [];
+
+	const partLen = 300;
+	for (let i = 0; i < tradeHeadList.length; i += partLen) {
+		const existTradeHeadPart = await supabase
+			.from('trade_head')
+			.select()
+			.in('trade_id', tradeHeadList.slice(i, i + partLen));
+		if (existTradeHeadPart.error) {
+			throw new Error(existTradeHeadPart.error.message);
+		} else {
+			// existTradeHead += existTradeHeadPart.data??[];
+			existTradeHeadPart.data?.forEach((e) => {
+				existTradeHead.push(e);
+			});
+		}
+	}
+
+	const noDupTradeHead = [...tradeHeadSet].filter((e) => {
+		return !existTradeHead.some((tradeHead) => tradeHead.trade_id === e.trade_id);
+	});
+	return noDupTradeHead;
+}
 
 async function filterNonExistentArtists(importedArtist: string[], importedTrade: ImportedTrade[]) {
 	const exist_artist = await getExistingArtists(importedArtist);
