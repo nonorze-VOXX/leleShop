c4e9ec24f4a3820beb42db7383a139cc968b2be6
nonorze-VOXX <appieosugood@gmail.com>
2025-03-04 16:29:22 +0800

    remove unuse function

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,7 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY } from '$env/static/public';
 import { type Database } from './db.types';
 // import { PRIVATE_SUPABASE_KEY, PRIVATE_SUPABASE_URL } from '$env/static/private';
-import { add } from './function/Utils';
 import DbArtistTrade from './db/DbArtistTrade';
 import DbCommission from './db/DbCommission';
 import DbStore from './db/DbStore';
@@ -29,34 +28,6 @@ export type SalesTotalData = {
 export const onePageLength = 100;
 
 export default {
-	async GetTradeTotalData(
-		id: string = '*',
-		date: { firstDate: Date | null; lastDate: Date | null } = { firstDate: null, lastDate: null }
-	) {
-		let query = supabase.from('artist_trade').select('total_sales,net_sales,discount'); //, sum(net_sales), sum(discount)');
-		if (id !== '*') {
-			query = query.eq('artist_id', id);
-		}
-		if (date.firstDate !== null && date.lastDate !== null) {
-			query = query
-				.gte('trade_date', date.firstDate.toISOString())
-				.lte('trade_date', date.lastDate.toISOString());
-		}
-
-		const { data, error } = await query;
-		if (error) {
-			console.error(error);
-		}
-		let total_sales_sum = 0;
-		let net_sales_sum = 0;
-		let discount_sum = 0;
-		if (data) {
-			total_sales_sum = data.map((el) => el.total_sales ?? 0).reduce(add);
-			net_sales_sum = data.map((el) => el.net_sales ?? 0).reduce(add);
-			discount_sum = data.map((el) => el.discount ?? 0).reduce(add);
-		}
-		return { total_sales_sum, net_sales_sum, discount_sum, error: null };
-	},
 	async GetArtistData(id: number | '*' = '*') {
 		let query = supabase.from('artist').select();
 		if (id !== '*') {
