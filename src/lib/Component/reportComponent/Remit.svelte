a2e64768f131c8664f26e5da647518aab39d9392
nonorze-VOXX <appieosugood@gmail.com>
2025-06-15 16:07:30 +0800

    hint the one store

--- src/lib/Component/reportComponent/Remit.svelte
+++ src/lib/Component/reportComponent/Remit.svelte
@@ -13,7 +13,7 @@
 	{#if commission !== null}
 		<InfoBox title="匯款金額" value={Math.ceil(net_total * ((100 - commission) / 100))}></InfoBox>
 	{:else}
-		<!-- <InfoBox title="匯款金額" value={'only support one store for this'}></InfoBox> -->
+		<InfoBox title="匯款金額" value={'請只選一個商店來看'}></InfoBox>
 	{/if}
 {:else}
 	<InfoBox title="匯款金額" value={'計算中'}></InfoBox>
