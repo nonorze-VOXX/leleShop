6114cdf4519f6c5d30550b2015fda1054d00d10a
green-Desktop <appieosugood@gmail.com>
2026-02-07 01:12:19 +0800

    make text color to red

--- src/lib/Component/InfoBox.svelte
+++ src/lib/Component/InfoBox.svelte
@@ -2,12 +2,17 @@
 	interface Props {
 		title: string;
 		value?: string | number;
+		text_color?: 'normal' | 'red';
 	}
 
-	let { title, value = '' }: Props = $props();
+	let { title, value = '', text_color = 'normal' }: Props = $props();
 </script>
 
-<div class="flex justify-between rounded-xl bg-lele-line p-2 text-lele-bg">
+<div
+	class="flex justify-between rounded-xl bg-lele-line p-2 {text_color === 'red'
+		? 'text-red-400'
+		: 'text-lele-bg'}"
+>
 	<p class="inline">
 		{title}
 		{#if value !== ''}:
