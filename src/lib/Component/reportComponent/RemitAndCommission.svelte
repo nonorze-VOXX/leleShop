d1f606bcbde38825a4d456a4fa36c886ad0d0760
nonorze-VOXX <appieosugood@gmail.com>
2026-02-27 00:58:55 +0800

    refactor: streamline commission fetching and improve data handling

--- src/lib/Component/reportComponent/RemitAndCommission.svelte
+++ src/lib/Component/reportComponent/RemitAndCommission.svelte
@@ -2,7 +2,6 @@
 	import { browser } from '$app/environment';
 	import { supabase } from '$lib/db';
 	import { selectedStore, type StoreList } from '$lib/store/choosing';
-	import { onDestroy, onMount } from 'svelte';
 
 	import Remit from './Remit.svelte';
 	import Commission from './Commission.svelte';
@@ -16,32 +15,32 @@
 	let { net_total, artist_id, year_month }: Props = $props();
 	let commission: number | null = $state(null);
 
-	const fetchCommission = async (e: StoreList) => {
-		if (browser) {
-			if (e !== '*' && e.length === 1) {
-				const { data, error } = await supabase
-					.from('default_commission_view')
-					.select('*')
-					.eq('store_name', e[0])
-					.eq('artist_id', artist_id)
-					.eq('year_month', year_month);
-				if (error) {
-					// console.error(error);
+	$effect(() => {
+		const load = async () => {
+			let e = $selectedStore;
+			if (browser) {
+				if (e !== '*' && e.length === 1) {
+					const { data, error } = await supabase
+						.from('default_commission_view')
+						.select('*')
+						.eq('store_name', e[0])
+						.eq('artist_id', artist_id)
+						.eq('year_month', year_month);
+					if (error) {
+						// console.error(error);
+						commission = null;
+						return;
+					}
+					if (data.length === 1) commission = data[0].commission;
+					else commission = 10; // default commission
+				} else {
 					commission = null;
-					return;
 				}
-				if (data.length === 1) commission = data[0].commission;
-				else commission = 10; // default commission
-			} else {
-				commission = null;
 			}
-		}
-	};
+		};
 
-	const unsubscribe = selectedStore.subscribe(fetchCommission);
-
-	onMount(async () => fetchCommission($selectedStore));
-	onDestroy(unsubscribe);
+		load();
+	});
 </script>
 
 <Commission {net_total} {commission} />
